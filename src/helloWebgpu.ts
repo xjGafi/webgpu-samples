cbff9b0e7d64ce5bafe0b64c567fc3258fbf7213
gaoyafei <gaoyafei@luojilab.com>
2022-04-06 13:39:51 +0800

    feat: update hello webgpu

--- src/helloWebgpu.ts
+++ src/helloWebgpu.ts
@@ -1,18 +1,43 @@
 // check webgpu support
 async function initWebGPU() {
   try {
-    if (!navigator.gpu)
-      throw new Error('Not support WebGPU')
-    const adapter = await navigator.gpu.requestAdapter()
-    if (!adapter)
-      throw new Error('No adapter found')
-    console.log(adapter)
-    document.body.innerHTML = '<h1>Hello WebGPU</h1>'
-    let i: keyof GPUSupportedLimits
-    for (i in adapter.limits)
-      document.body.innerHTML += `<p>${i}:${adapter.limits[i]}</p>`
+    // GPU
+    const { gpu } = navigator
+    if (!gpu) {
+      throw new Error("No support WebGPU");
+    }
+    document.body.innerHTML = "<h1>Hello WebGPU</h1>"
+
+    // 适配器
+    const adapter = await gpu.requestAdapter()
+    if (!adapter) {
+      throw new Error("No adapter found");
+    }
+    console.log('🌈 adapter:', adapter);
+    document.body.innerHTML += "<h2>👀 adapter</h2>"
+
+    // let x: keyof GPUSupportedLimits;
+    // for (x in adapter.limits) {
+    //   document.body.innerHTML += `<p>${x}:${adapter.limits[x]}</p>`
+    // }
+
+    // 设备
+    const device = await adapter.requestDevice();
+    if (!device) {
+      throw new Error("No device found");
+    }
+    // console.log('🌈 device:', device);
+    document.body.innerHTML += "<h2>👀 device</h2>"
+
+    // let y: keyof GPUSupportedLimits;
+    // for (y in device.limits) {
+    //   document.body.innerHTML += `<p>${y}:${device.limits[y]}</p>`
+    // }
+
   } catch (error: any) {
     document.body.innerHTML = `<h1>${error.message}</h1>`
+    console.error('🌈 error:', error);
   }
 }
+
 initWebGPU()
