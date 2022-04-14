7f66344ab9e2f1842407b9f6946c116fe45f3f02
gaoyafei <gaoyafei@luojilab.com>
2022-04-15 01:38:19 +0800

    feat: history router

--- src/common/router.ts
+++ src/common/router.ts
@@ -11,26 +11,104 @@ class Router {
     this.init();
   }
 
-  register(path: string, callback: Function) {
-    // 给不同的 hash 设置不同的回调函数
-    this.routers[path] = callback || function () { };
-    console.log('🌈 this.routers:', this.routers);
-  }
+  // register(path: string, callback: Function) {
+  //   // 给不同的 hash 设置不同的回调函数
+  //   this.routers[path] = callback || function () { };
+  //   console.log('🌈 this.routers:', this.routers);
+  // }
 
-  refresh() {
-    // 如果存在 hash 值则获取到，否则设置 hash 值为 /
-    this.currentPath = location.hash.slice(1) || '';
+  // refresh() {
+  //   // 如果存在 hash 值则获取到，否则设置 hash 值为 /
+  //   this.currentPath = location.hash.slice(1) || '';
 
-    // 根据当前的 hash 值来调用相对应的回调函数
-    if (this.currentPath !== '/') {
-      this.routers[this.currentPath]();
-    }
-  }
+  //   // 根据当前的 hash 值来调用相对应的回调函数
+  //   if (this.currentPath !== '/') {
+  //     this.routers[this.currentPath]();
+  //   }
+  // }
 
   init() {
-    window.addEventListener('load', this.refresh.bind(this), false);
-    window.addEventListener('hashchange', this.refresh.bind(this), false);
+    window.addEventListener(
+      'load',
+      () => {
+        let path = location.pathname;
+        this.assign.call(this, path);
+      },
+      false
+    );
+
+    window.addEventListener(
+      'popstate',
+      (event: PopStateEvent) => {
+        const state = event.state || {};
+        const path = state.path || '';
+        this.refresh.call(this, path);
+      },
+      false
+    );
+
+    const router = document.querySelector<HTMLUListElement>('#router')!;
+    router.addEventListener(
+      'click',
+      (event: MouseEvent) => {
+        const { dataset: { path } } = event.target as any;
+
+        this.currentPath = location.pathname;
+        if (this.currentPath === path) return;
+
+        event.preventDefault();
+        this.assign.call(this, path);
+      },
+      false
+    );
   }
+
+  // 用于注册每个视图
+  register(path: string, callback = function () { }) {
+    this.routers[path] = callback;
+  }
+
+  // 跳转到 path
+  assign(path: string) {
+    console.log('🌈 assign path:', path);
+
+    history.pushState({ path }, '', path);
+    this.refresh(path);
+  }
+
+  // 替换为 path
+  replace(path: string) {
+    console.log('🌈 replace path:', path);
+
+    history.replaceState({ path }, '', path);
+    this.refresh(path);
+  }
+
+  // 通用处理 path 调用回调函数
+  refresh(path: string) {
+    console.log('🌈 refresh path:', path);
+
+    let handler;
+    // 没有对应 path
+    if (!this.routers.hasOwnProperty(path)) {
+      handler = this.routers['404'] || function () { };
+    }
+    // 有对应 path
+    else {
+      handler = this.routers[path];
+    }
+
+    console.log('🌈 handler:', handler);
+
+    try {
+      handler.call(this);
+    } catch (error) {
+      console.error('🤯', error);
+      (this.routers['error'] || function () { }).call(this, error);
+    }
+
+  }
+
 }
 
 export default Router;
--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,20 @@ const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
-  <div id="click_btn">
-    <a href="#/one">第一个页面</a>
-    <a href="#/two">第二个页面</a>
-    <a href="#/three">第三个页面</a>
-    <a href="#/helloWebgpu">helloWebgpu</a>
-    <a href="#/basicTriangle">basicTriangle</a>
-  </div>
+  <ul id="router">
+    <li data-path="/one">第一个页面</li>
+    <li data-path="/two">第二个页面</li>
+    <li data-path="/three">第三个页面</li>
+    <li data-path="/helloWebgpu">helloWebgpu</li>
+    <li data-path="/basicTriangle">basicTriangle</li>
+  </ul>
   <div class="content"></div>
   <canvas id="sketchpad"></canvas>
 `
 
 const router = new Router();
 
+router.register('/', () => import("./one"));
 router.register('/one', () => import("./one"));
 router.register('/two', () => import("./two"));
 router.register('/three', () => import("./three"));
